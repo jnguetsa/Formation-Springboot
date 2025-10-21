5e2cbd170ee4082fa3891ba749300774081fc643
JUNIOR NOUMEDEM <juniornoumedem02@gmail.com>
2025-10-21 18:56:13 +0100

    Feature : essaie de connexion dans la BD Docker+comose +Postgres-alpine

---
+++ src/main/java/com/example/formation/Entites/Administrateur.java
@@ -0,0 +1,10 @@
+package entites;
+
+
+public class Administrateur extends Utilisateur{
+    public Administrateur(int id , String nom, String email, String telephone) {
+        super(id, nom, email, telephone);
+    }
+
+
+}
